c2b6d67a74f3061a5cef246213b16997de7f689a
Sam Jones <sam@genomoncology.com>
2015-10-02 09:23:53 -0400

    Fixing url for form

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002093356) do
-
-  create_table "calculations", force: :cascade do |t|
-    t.decimal "initial_value"
-    t.string  "addition_value"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
